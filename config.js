8ab1cb1893f943ffc435c3e4e0179019c24eed5e
hphucdeveloper <125047129+hphucdeveloper@users.noreply.github.com>
2023-02-14 10:57:48 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Các con vợ';
+const giftUrl = 'https://www.facebook.com/HPhuc.Developers';
+const eventName = 'Chúc Mừng 14-2';
+const titleCard = 'Tặng các con vợ';
+const contentCard = 'Chúc các con vợ của anh 14/2 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr.HPhuc https://www.facebook.com/HPhuc.Developers
+const giftImage = 'happy-valentine-gif-3.gif';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
